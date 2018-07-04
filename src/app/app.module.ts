67122dd421214bf3fc5dbdd4a3bc415e6693ea9c
mrhrao <hemant.rao@oodlestechnologies.com>
2018-07-04 19:13:59 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AuthenticatedHttpService } from './interceptor/api-interceptor';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import {ToastModule,ToastOptions} from 'ng2-toastr/ng2-toastr';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 // import { ChartModule } from 'angular2-highcharts';
 import { appRoutes } from './app.routes';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
@@ -96,6 +98,10 @@ import {PaymentService} from './payment/payment.service';
     ReactiveFormsModule
   ],
   providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    },
     {provide: ToastOptions, useClass: CustomOption},
     HeaderService,
     VerificationService,
